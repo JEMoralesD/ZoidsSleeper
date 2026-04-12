fb292c29ec8a427314cfa14b1a1d9d1147c849d3
JEMoralesD <jemd92@hotmail.com>
2026-04-12 15:30:50 -0600

    Rebalance first bandit battle (#60)

---
+++ test/Pilot.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+import { PILOTS } from '../src/models/Pilot';
+import { buildZoid } from '../src/models/Zoid';
+
+describe('Pilot', () => {
+  describe('bandit1', () => {
+    const bandit = PILOTS['bandit1'];
+    const molga = buildZoid(bandit.zoids[0]);
+
+    it('should have Molga with attack of 1', () => {
+      expect(molga.attack).toBe(1);
+    });
+
+    it('should have Molga with maxHealth of 196', () => {
+      expect(molga.maxHealth).toBe(196);
+    });
+
+    it('should reward 3000 magnis', () => {
+      expect(bandit.magnisReward).toBe(3000);
+    });
+  });
+});
